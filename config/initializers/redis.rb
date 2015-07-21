5c87872c3118d642e7a316685a2d1dfedeab0148
Zachariah Ngonyani <zachariahn@gmail.com>
2015-07-21 19:02:25 +0300

    configure redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,1 @@
-$redis = Redis.new(:driver => :hiredis)
+if Rails.env.development?   $redis = Redis.new(:driver => :hiredis)else  uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)end
