0e3f92bdd7808192a481820404b258f68e84a299
Zachariah Ngonyani <zachariahn@gmail.com>
2015-07-22 03:01:53 +0300

    Fixed redis setup

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,1 @@
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )# $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :driver => :hiredis)$redis = Redis.new(:driver => :hiredis, :host => uri.host)
+# uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )# $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :driver => :hiredis)$redis = Redis.new(:driver => :hiredis)
